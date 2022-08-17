c48e79e857c0106a3b7e0e7ccbe09c75f071fb4d
mafparra <mafernandezparra@gmail.com>
2022-08-17 14:50:41 -0400

    Testing about render data with event onclick and Card, adding information about specialization getting

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,21 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 import { Navbar, NavbarBrand } from 'reactstrap';
 import React, { Component } from 'react';
 
-import CertificateList from './components/CertificatesComponent';
+import CertificatesList from './components/CertificatesComponent';
+import { CERTIFICATES } from './shared/certificates_list';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      certificates: CERTIFICATES
+    }
+  }
   render() {
     return (
-      <div className="App">
+      <div>
         <Navbar dark color="primary">
           <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+            <NavbarBrand href="/">Marco Antonio Parra, Portfolio</NavbarBrand>
           </div>
         </Navbar>
-        <CertificateList />
+        <CertificatesList certificates={this.state.certificates} />
       </div>
     );
   }
